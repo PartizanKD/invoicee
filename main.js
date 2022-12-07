31b2591a15dbba54b79e340264cf972667ebe218
PartizanKD <kaschindmitrij@yandex.ru>
2022-12-07 08:25:26 +0300

    LocalStorage, only numbers and max numbers for Number, Discount and Taxes

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-const domNumber = document.getElementById('invoiceNumber');
+const domInvoiceNumber = document.getElementById('invoiceNumber');
+const domInvoiceDiscount = document.getElementById('invoiceDiscount');
+const domInvoiceTaxes = document.getElementById('invoiceTaxes');
 
-domNumber.value = JSON.parse(localStorage.getItem('test'));
+domInvoiceNumber.value = JSON.parse(localStorage.getItem('keyNumber'));
+domInvoiceDiscount.value = JSON.parse(localStorage.getItem('keyDiscount'));
+domInvoiceTaxes.value = JSON.parse(localStorage.getItem('keyTaxes'));
 
-//const LOCAL_NUMBER = domNumber.value;
+domInvoiceNumber.addEventListener('keyup', () => {localStorageOfInvoice(domInvoiceNumber.value, 'keyNumber')} );
+domInvoiceDiscount.addEventListener('keyup', () => {localStorageOfInvoice(domInvoiceDiscount.value, 'keyDiscount')} );
+domInvoiceTaxes.addEventListener('keyup', () => {localStorageOfInvoice(domInvoiceTaxes.value, 'keyTaxes')} );
+domInvoiceNumber.addEventListener('keydown', (event) =>{onlyNumbersAndMax(event, domInvoiceNumber.value, 9999)} );
+domInvoiceDiscount.addEventListener('keydown', (event) =>{onlyNumbersAndMax(event, domInvoiceDiscount.value, 99)} );
+domInvoiceTaxes.addEventListener('keydown', (event) =>{onlyNumbersAndMax(event, domInvoiceTaxes.value, 99)} );
 
-domNumber.addEventListener('keyup', invoiceNumber);
-function invoiceNumber() {
-    let LOCAL_NUMBER = domNumber.value;
-    localStorage.setItem('test', JSON.stringify(LOCAL_NUMBER));
-    console.log(JSON.parse(localStorage.getItem('test')));
+function localStorageOfInvoice(value, key) {
+    localStorage.setItem(key, JSON.stringify(value));
+    console.log(JSON.parse(localStorage.getItem(key)));
+}
+
+function onlyNumbersAndMax(event, value,  max) {
+    if ((event.key >= 0 && event.key <= 9 && (value + event.key) <= max && event.key !== ' ')
+        || event.key === 'Backspace' || event.key === 'Delete'
+        || event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+
+    } else {
+        event.preventDefault();
+    }
 }
